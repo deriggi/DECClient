002d88577410b9ed0b29ae9845cfad2932268322
deriggi <john.deriggi@gmail.com>
2015-07-15 15:14:42 -0400

    save files and some user interaction

--- decpulller.py
+++ decpulller.py
@@ -5,6 +5,16 @@ from io import StringIO
 import urllib
 
 def constructQuery(term, format):
+	""" builds the encoded query for the API call
+
+	Args
+		term: 	the query string
+		format: the response format, CSV, JSON, XML...
+
+	Returns
+		api_call: the url for the response  
+
+	"""
 	baseUrl = "https://dec.usaid.gov/api/qsearch.ashx?"
 	encoded = str(base64.b64encode(term))
 	encoded = encoded[2:len(encoded)-1]
@@ -50,12 +60,25 @@ def saveResponse(data, filename):
 # saveResponse(res_obj[0], res_obj[1])
 
 ## calling dec api
-dec_response = makeGetRequest(constructQuery(b"Haiti and climate", "JSON"))
+search_string = input("please enter a query for the Development Experience Clearninghouse: ")
+dec_response = makeGetRequest(constructQuery(bytearray(search_string, 'utf-8'), "JSON"))
 io = StringIO( dec_response[0].decode('utf-8') )
 dec_obj = json.load(io)
+print("{0} records found!".format(dec_obj['RecordsFound']));
 
+
+file_count = 0
 for record in dec_obj['Records']:
-	print(record['File']['value'])
+
+	for i in record['File']['value']:
+		file_count = file_count + 1;
+		res = makeGetRequest(i)
+		filename = res[1]
+		if filename.rfind('.pdf') != len(filename) - 4:
+			filename = 'decFile_' + str(file_count) + '.pdf'
+			# saveResponse(res[0], res[1])
+		saveResponse(res[0], "downloads/" + filename)
 
 # numRecords = str(len(dec_obj['Records']))
 # print ( str(dec_obj['RecordsFound'] )+ " should equal " + numRecords )
+
